a855b6525ff60d41daf87e9019ff9876cc3a29e7
Ahmed Fekry <ahmedfekry792@gmail.com>
2024-02-08 00:27:51 +0200

    adding auto Login Feature)

--- app/src/main/java/com/example/mealanner/UILayer/SplashScreenActivity/splashscreen.java
+++ app/src/main/java/com/example/mealanner/UILayer/SplashScreenActivity/splashscreen.java
@@ -9,9 +9,14 @@ import android.os.Handler;
 import com.example.mealanner.R;
 import com.example.mealanner.UILayer.AppMainActivity.MainActivity;
 import com.example.mealanner.UILayer.LoginActivity.Login;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class splashscreen extends AppCompatActivity {
     private static final int SPLASH_TIMEOUT = 3000;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUser user = auth.getCurrentUser();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,18 @@ public class splashscreen extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                // Start the main activity after the splash timeout
-                Intent intent = new Intent(splashscreen.this, Login.class);
-                startActivity(intent);
-                finish();  // Close the splash activity to prevent going back to it
+                if (user != null) {
+                    // Start the main activity after the splash timeout
+                    Intent intent = new Intent(splashscreen.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    Intent intent = new Intent(splashscreen.this, Login.class);
+                    startActivity(intent);
+                    finish();
+
+                }
+                // Close the splash activity to prevent going back to it
             }
         }, SPLASH_TIMEOUT);
     }
