d376caffeefab30970681c77a422ac26549f2d2b
Ahmed Fekry <ahmedfekry792@gmail.com>
2024-02-10 04:13:12 +0200

    Adding country and Category Filters Feature

--- app/src/main/java/com/example/mealanner/UILayer/AppMainActivity/SearchFragment/SearchFragment.java
+++ app/src/main/java/com/example/mealanner/UILayer/AppMainActivity/SearchFragment/SearchFragment.java
@@ -2,59 +2,68 @@ package com.example.mealanner.UILayer.AppMainActivity.SearchFragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.LiveData;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Toast;
 
+import com.example.mealanner.DataLayer.Model.DataModels.Categories;
+import com.example.mealanner.DataLayer.Model.DataModels.Category;
+import com.example.mealanner.DataLayer.Model.DataModels.Countries;
+import com.example.mealanner.DataLayer.Model.DataModels.Country;
+import com.example.mealanner.DataLayer.Model.DataModels.Meal;
+import com.example.mealanner.DataLayer.Model.DataModels.Meals;
+import com.example.mealanner.DataLayer.Model.Services.Local.LocalDataSourceImpl;
+import com.example.mealanner.DataLayer.Model.Services.Remote.NetworkCallBack;
+import com.example.mealanner.DataLayer.Model.Services.Remote.RemoteDataSourceImpl;
+import com.example.mealanner.DataLayer.Model.Services.Remote.Repository;
+import com.example.mealanner.DataLayer.Model.Services.Remote.RepositoryImpl;
 import com.example.mealanner.R;
+import com.example.mealanner.UILayer.AppMainActivity.HomeFragment.Presenter.HomePresenter;
+import com.example.mealanner.UILayer.AppMainActivity.HomeFragment.View.CategoriesRCAdapter;
+import com.example.mealanner.UILayer.AppMainActivity.HomeFragment.View.CountriesRCAdapter;
+import com.example.mealanner.UILayer.AppMainActivity.HomeFragment.View.HomeFragment;
+import com.example.mealanner.UILayer.AppMainActivity.HomeFragment.View.HomeView;
+import com.example.mealanner.UILayer.AppMainActivity.MealsFragment.Presenter.MealsPresenter;
+import com.example.mealanner.UILayer.AppMainActivity.MealsFragment.View.MealsFragment;
+import com.example.mealanner.UILayer.AppMainActivity.MealsFragment.View.MealsRCAdapter;
+import com.example.mealanner.UILayer.AppMainActivity.MealsFragment.View.MealsView;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class SearchFragment extends Fragment {
+import java.util.List;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class SearchFragment extends Fragment implements NetworkCallBack, HomeView, MealsView {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    ChipGroup chipGroup;
+    RecyclerView searchRC;
+    HomePresenter homePresenter;
+    MealsPresenter mealsPresenter;
+    Repository repository;
+    CategoriesRCAdapter categoriesRCAdapter;
+    CountriesRCAdapter countriesRCAdapter;
+    MealsRCAdapter mealsRCAdapter;
+    Categories localCategories;
+    Countries localCountries;
 
-    public SearchFragment() {
-        // Required empty public constructor
-    }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -62,5 +71,120 @@ public class SearchFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        chipGroup = view.findViewById(R.id.chipsGroupID);
+        searchRC = view.findViewById(R.id.searchRC);
+        repository = RepositoryImpl.getInstance(RemoteDataSourceImpl.getInstance(Void.class), LocalDataSourceImpl.getInstance(getContext().getApplicationContext()));
+        homePresenter = new HomePresenter(repository, SearchFragment.this);
+        mealsPresenter = new MealsPresenter(repository , SearchFragment.this);
+        setupChips();
+        homePresenter.getCountries();
+        homePresenter.getCategories();
+
+    }
+    void setupChips(){
+        for (int i=0; i<chipGroup.getChildCount();i++)
+        {
+            Chip chip = (Chip) chipGroup.getChildAt(i);
+            chip.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    Log.i("TAG", "onCheckedChanged: click");
+                    if(isChecked)
+                    {
+                        Log.i("TAG", "onCheckedChanged: check");
+                        if (chip.getText().toString().equals("country"))
+                        {
+                            Log.i("TAG", "onCheckedChanged: country");
+                            showCountries(localCountries);
+                            searchRC.setAdapter(countriesRCAdapter);
+                            countriesRCAdapter.notifyDataSetChanged();
+                        } else if (chip.getText().toString().equals("category")) {
+                            Log.i("TAG", "onCheckedChanged: category");
+                            showCategories(localCategories);
+                            searchRC.setAdapter(categoriesRCAdapter);
+                            categoriesRCAdapter.notifyDataSetChanged();
+
+
+                        }else if (chip.getText().toString().equals("ingredient")) {
+                            Log.i("TAG", "onCheckedChanged: ingredient");
+                           // adapter.setList(getSony());
+                           // adapter.notifyDataSetChanged();
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onSuccess(Object result) {
+
+    }
+
+    @Override
+    public void onFailure(String errorMsg) {
+
+    }
+
+    @Override
+    public void showRandomMeal(Meals result) {
+
+    }
+
+    @Override
+    public void showCountries(Countries result) {
+        Log.i("yala", "showCountries: " +result.getMeals().get(0).getStrArea());
+        countriesRCAdapter = new CountriesRCAdapter(getContext() ,result.getMeals());
+        countriesRCAdapter.setOnItemClickListener(country -> onCountryClick(country));
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        searchRC.setLayoutManager(gridLayoutManager);
+        localCountries = result;
+    }
+
+    @Override
+    public void showCategories(Categories result) {
+        categoriesRCAdapter = new CategoriesRCAdapter(getContext() ,result.getCategories());
+        categoriesRCAdapter.setOnItemClickListener(category -> onCategoryClick(category));
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
+        searchRC.setLayoutManager(gridLayoutManager);
+        localCategories = result;
+    }
+    public void onCategoryClick(Category category) {
+        mealsPresenter.getMealsByCategories(category.getStrCategory());
+    }
+    public void onCountryClick(Country country) {
+        mealsPresenter.getMealsByCountries(country.getStrArea());
+
+    }
+    private void updateRecyclerViewWithMealsAdapter(RecyclerView.Adapter adapter) {
+        searchRC.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+        StaggeredGridLayoutManager gridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        searchRC.setLayoutManager(gridLayoutManager);
+    }
+
+    @Override
+    public void showMealsByCategory(Meals result) {
+        mealsRCAdapter = new MealsRCAdapter(getContext(), result.getMeals());
+        updateRecyclerViewWithMealsAdapter(mealsRCAdapter);
+
+
+    }
+
+    @Override
+    public void showMealsByCountry(Meals result) {
+        mealsRCAdapter = new MealsRCAdapter(getContext() ,result.getMeals());
+        updateRecyclerViewWithMealsAdapter(mealsRCAdapter);
+
+    }
+
+    @Override
+    public void showSavedMeals(LiveData<List<Meal>> meals) {
+
     }
 }
